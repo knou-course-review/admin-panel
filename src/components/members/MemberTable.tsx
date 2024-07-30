aca3883da111e042c3a2da9055642ede61937b87
jbilee <julishahn@gmail.com>
2024-07-30 19:33:22 +0900

    refactor: 유저 목록 테이블 컴포넌트 리팩토링 - 유저 정지 모달창을 사용할 수 있도록 전달 받는 prop 구조 변경

--- src/components/members/MemberTable.tsx
+++ src/components/members/MemberTable.tsx
@@ -1,28 +1,42 @@
 "use client";
 
-import MemberTableRow, { MemberTableRowProps } from "./MemberTableRow";
+import { useState } from "react";
+import MemberTableRow, { type MemberData } from "./MemberTableRow";
+import MemberBanModal from "./MemberBanModal";
 
 export default function MemberTable() {
+  const [isShowing, setIsShowing] = useState(false);
+  const [selectedMember, setSelectedMember] = useState<null | string>(null);
+  const openModal = (username: string) => {
+    setSelectedMember(username);
+    setIsShowing(true);
+  };
+  const closeModal = () => setIsShowing(false);
   return (
-    <table className="table-fixed w-full text-center">
-      <thead>
-        <tr className="border border-b-2 font-bold border-r-neutral-300 border-l-neutral-300 border-t-neutral-300 border-neutral-400 bg-neutral-300">
-          <td className="p-4">아이디</td>
-          <td className="p-4">이메일</td>
-          <td className="p-4 w-32">상태</td>
-          <td className="w-20"></td>
-        </tr>
-      </thead>
-      <tbody>
-        {sampleData.map((data) => (
-          <MemberTableRow key={data.id} {...data} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table className="table-fixed w-full text-center">
+        <thead>
+          <tr className="border border-b-2 font-bold border-r-neutral-300 border-l-neutral-300 border-t-neutral-300 border-neutral-400 bg-neutral-300">
+            <td className="p-4">아이디</td>
+            <td className="p-4">이메일</td>
+            <td className="p-4 w-32">상태</td>
+            <td className="w-20"></td>
+          </tr>
+        </thead>
+        <tbody>
+          {sampleData.map((data) => (
+            <MemberTableRow key={data.id} userData={{ ...data }} openModal={openModal} />
+          ))}
+        </tbody>
+      </table>
+      {isShowing && selectedMember && (
+        <MemberBanModal isShowing={isShowing} username={selectedMember} closeModal={closeModal} />
+      )}
+    </>
   );
 }
 
-const sampleData: MemberTableRowProps[] = [
+const sampleData: MemberData[] = [
   {
     id: 1,
     username: "asdf41f6a5w4",
--- src/components/members/MemberTableRow.tsx
+++ src/components/members/MemberTableRow.tsx
@@ -5,21 +5,26 @@ import { IconButton } from "@mui/material";
 
 type MemberStatus = "정상" | "정지";
 
-export type MemberTableRowProps = {
+export type MemberData = {
   id: number;
   username: string;
   email: string;
   status: MemberStatus;
 };
 
-export default function MemberTableRow(props: MemberTableRowProps) {
+export type MemberTableRowProps = {
+  userData: MemberData;
+  openModal: (username: string) => void;
+};
+
+export default function MemberTableRow({ userData, openModal }: MemberTableRowProps) {
   return (
     <tr className="border border-neutral-400">
-      <td className="p-2">{props.username}</td>
-      <td className="p-2">{props.email}</td>
-      <td className="p-2">{props.status}</td>
+      <td className="p-2">{userData.username}</td>
+      <td className="p-2">{userData.email}</td>
+      <td className="p-2">{userData.status}</td>
       <td className="p-2">
-        <IconButton>
+        <IconButton onClick={() => openModal(userData.username)}>
           <Block fontSize="small" />
         </IconButton>
       </td>
