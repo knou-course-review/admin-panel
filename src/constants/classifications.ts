45e4e5229fc50f59d0f07931d4b5319aa893450c
jbilee <julishahn@gmail.com>
2024-08-01 16:10:21 +0900

    chore: 교과 및 학과 정보 상수로 등록

---
+++ src/constants/classifications.ts
@@ -0,0 +1,5 @@
+export const CLASSIFICATIONS = {
+  major: "전공",
+  elective: "교양",
+  general: "일반선택",
+};
---
+++ src/constants/departments.ts
@@ -0,0 +1,29 @@
+export const LIBERAL_ARTS = {
+  department: "인문과학대학",
+  programs: ["국어국문학과", "영어영문학과", "중어중문학과", "프랑스언어문화학과", "일본학과"],
+};
+
+export const SOCIAL_SCIENCES = {
+  department: "사회과학대학",
+  programs: [
+    "법학과",
+    "행정학과",
+    "경제학과",
+    "경영학과",
+    "무역학과",
+    "미디어영상학과",
+    "관광학과",
+    "사회복지연계전공",
+    "사회복지학과",
+  ],
+};
+
+export const NATURAL_SCIENCES = {
+  department: "자연과학대학",
+  programs: ["농학과", "생활과학부", "컴퓨터과학과", "통계•데이터과학과", "보건환경학과", "간호학과"],
+};
+
+export const EDUCATIONAL_SCIENCES = {
+  department: "교육과학대학",
+  programs: ["교육학과", "청소년교육과", "유아교육과", "문화교양학과", "생활체육지도과"],
+};
