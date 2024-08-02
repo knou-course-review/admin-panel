27450b4b05317bac8dd962dcdcdcd28f7b3e795b
jbilee <julishahn@gmail.com>
2024-08-03 08:12:41 +0900

    feat: NavBar 컴포넌트 메뉴 추가 - 메뉴를 액션별로 세분화 - 학과 및 교수 관련 페이지로 이동하는 링크 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,76 @@
 "use client";
 
 import Link from "next/link";
-import { Logout } from "@mui/icons-material";
+import {
+  AccountCircle,
+  Domain,
+  DomainAdd,
+  Group,
+  GroupAdd,
+  Groups,
+  LibraryBooksOutlined,
+  Logout,
+  PostAdd,
+} from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 
 export default function NavBar() {
   const logout = () => {};
   return (
     <div className="flex flex-col w-52 max-h-dvh p-6 list-none bg-neutral-950 text-white">
-      <div className="mb-8 font-bold">user_</div>
-      <li className="mb-4">
-        <Link href="/courses">강의 목록</Link>
-      </li>
-      <li>
-        <Link href="/members">회원 목록</Link>
-      </li>
+      <div className="mb-8 font-bold">
+        <AccountCircle fontSize="small" /> admin_12345
+      </div>
+      <div className="flex flex-col gap-2 mb-6">
+        <div>
+          <span className="font-semibold text-sm text-slate-300">조회</span>
+        </div>
+        <li>
+          <Link href="/departments">
+            <Domain fontSize="small" /> 학과 목록
+          </Link>
+        </li>
+        <li>
+          <Link href="/professors">
+            <Group fontSize="small" /> 교수 목록
+          </Link>
+        </li>
+        <li>
+          <Link href="/courses">
+            <LibraryBooksOutlined fontSize="small" /> 강의 목록
+          </Link>
+        </li>
+      </div>
+      <div className="flex flex-col gap-2 mb-6">
+        <div>
+          <span className="font-semibold text-sm text-slate-300">신규 등록</span>
+        </div>
+        <li>
+          <Link href="/departments/new">
+            <DomainAdd fontSize="small" /> 학과 등록
+          </Link>
+        </li>
+        <li>
+          <Link href="/professors/new">
+            <GroupAdd fontSize="small" /> 교수 등록
+          </Link>
+        </li>
+        <li>
+          <Link href="/courses/new">
+            <PostAdd fontSize="small" /> 강의 등록
+          </Link>
+        </li>
+      </div>
+      <div className="flex flex-col gap-2 mb-6">
+        <div>
+          <span className="font-semibold text-sm text-slate-300">관리</span>
+        </div>
+        <li>
+          <Link href="/members">
+            <Groups fontSize="small" /> 회원 관리
+          </Link>
+        </li>
+      </div>
       <li className="mt-auto self-end">
         <IconButton onClick={logout}>
           <Logout sx={{ color: "white" }} />
