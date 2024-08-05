6f83aee0c921c122d7202557ee2fba18d188bc2f
jbilee <julishahn@gmail.com>
2024-08-06 01:04:44 +0900

    feat: 강의 등록 페이지 구현

---
+++ src/app/courses/new/page.tsx
@@ -0,0 +1,10 @@
+import NewCourseForm from "@/components/courses/NewCourseForm";
+
+export default function NewCourse() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-8">강의 등록</h1>
+      <NewCourseForm />
+    </div>
+  );
+}
