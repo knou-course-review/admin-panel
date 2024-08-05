58068decc085070c32bedd62befd1f4604b8aa45
jbilee <julishahn@gmail.com>
2024-08-05 18:56:06 +0900

    feat: 교수 목록 페이지 구현

---
+++ src/app/professors/page.tsx
@@ -0,0 +1,10 @@
+import ProfessorsContent from "@/components/professors/ProfessorsContent";
+
+export default function Professors() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-8">교수 목록</h1>
+      <ProfessorsContent />
+    </div>
+  );
+}
