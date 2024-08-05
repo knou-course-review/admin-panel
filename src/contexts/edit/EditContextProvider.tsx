f1a64fe4d3611e863483fb6c2d58d52d946467bf
jbilee <julishahn@gmail.com>
2024-08-06 02:43:43 +0900

    feat: 수정 데이터 context 구현 - 수정할 데이터의 원본을 context에 저장하도록 구현

---
+++ src/contexts/edit/EditContextProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { createContext, useState, type ReactNode } from "react";
+import type { DepartmentData } from "@/components/departments/DepartmentsContent";
+import type { ProfessorData } from "@/components/professors/ProfessorsContent";
+
+export type EditContextProps = EditContextState;
+
+export type EditContextState = {
+  data: EditContextData | null;
+  updateData: (newData: EditContextData) => void;
+};
+
+export type EditContextData = ProfessorData | DepartmentData;
+
+export const EditContext = createContext<EditContextProps>({ data: null, updateData: () => {} });
+
+export const EditContextProvider = ({ children }: { children: ReactNode }) => {
+  const [data, setData] = useState<EditContextData | null>(null);
+  const updateData = (newData: EditContextData) => setData(newData);
+  return <EditContext.Provider value={{ data, updateData }}>{children}</EditContext.Provider>;
+};
