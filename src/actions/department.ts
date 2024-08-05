5717877a72cc90cb41d412012cfd9a75bf5b6e4a
jbilee <julishahn@gmail.com>
2024-08-05 16:41:30 +0900

    feat: 학과 생성 서버 액션 구현

---
+++ src/actions/department.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { api } from "@/utils/api";
+import { getSession } from "@/lib/auth";
+import { DepartmentFormSchema } from "@/schema/department";
+
+export async function addDepartment(formData: FormData) {
+  // Return early if not admin
+  const userSession = await getSession();
+  // TODO: Decrypt token here and check expiry & admin role
+  if (!userSession) return { isValid: false };
+
+  const form = { departmentName: formData.get("departmentName") };
+  const validatedForm = DepartmentFormSchema.safeParse(form);
+  if (!validatedForm.success) {
+    return { errors: validatedForm.error.flatten().fieldErrors };
+  }
+
+  if (validatedForm.success) {
+    const data = validatedForm.data;
+    try {
+      const res = await api.post("/api/v1/department", data, userSession.token);
+      const body = await res.json();
+      console.log(body);
+      if (body.code === 200) {
+        return { isValid: true, data: body.data };
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return { isValid: false };
+}
