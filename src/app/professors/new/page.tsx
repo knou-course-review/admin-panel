024dfde9c51f585562fad9a440406af16656560c
jbilee <julishahn@gmail.com>
2024-08-05 18:25:33 +0900

    feat: 교수 생성 페이지 구현

---
+++ src/app/professors/new/page.tsx
@@ -0,0 +1,10 @@
+import NewProfessorForm from "@/components/professors/NewProfessorForm";
+
+export default function NewProfessor() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-8">교수 등록</h1>
+      <NewProfessorForm />
+    </div>
+  );
+}
