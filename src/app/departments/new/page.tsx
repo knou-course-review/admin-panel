9f494ee50e5d4be67e395e21370b02cc145024a2
jbilee <julishahn@gmail.com>
2024-08-05 16:41:46 +0900

    feat: 학과 생성 페이지 구현

---
+++ src/app/departments/new/page.tsx
@@ -0,0 +1,10 @@
+import NewDepartmentForm from "@/components/departments/NewDepartmentForm";
+
+export default function NewDepartment() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-8">학과 등록</h1>
+      <NewDepartmentForm />
+    </div>
+  );
+}
