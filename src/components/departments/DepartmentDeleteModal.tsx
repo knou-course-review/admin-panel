61064b05595cd068b1d1ae976ee51c120e5fd23c
jbilee <julishahn@gmail.com>
2024-08-05 19:04:50 +0900

    feat: 학과 삭제 모달창 컴포넌트 구현

---
+++ src/components/departments/DepartmentDeleteModal.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button } from "@mui/material";
+import ModalFrame from "../ModalFrame";
+
+type DepartmentDeleteModalProps = {
+  isShowing: boolean;
+  id: string;
+  departmentName: string;
+  closeModal: () => void;
+};
+
+export default function DepartmentDeleteModal({
+  isShowing,
+  id,
+  departmentName,
+  closeModal,
+}: DepartmentDeleteModalProps) {
+  const handleDelete = () => {
+    // Request to server
+    closeModal();
+  };
+
+  if (!isShowing) return null;
+  return (
+    <ModalFrame title="학과 삭제" closeModal={closeModal}>
+      <div className="flex flex-col w-96 gap-4 px-4 text-center">
+        <p>아래 학과를 데이터베이스에서 삭제합니다.</p>
+        <p>{departmentName}</p>
+        <Button variant="contained" fullWidth onClick={handleDelete}>
+          삭제
+        </Button>
+      </div>
+    </ModalFrame>
+  );
+}
