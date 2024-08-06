c33f346138353e82061cce8a274663404f928317
jbilee <julishahn@gmail.com>
2024-08-06 10:41:07 +0900

    feat: 학과 정보 수정 페이지 구현

---
+++ src/app/departments/edit/[id]/page.tsx
@@ -0,0 +1,10 @@
+import EditDepartmentForm from "@/components/departments/EditDepartmentForm";
+
+export default function EditDepartment() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-8">학과 정보 수정</h1>
+      <EditDepartmentForm />
+    </div>
+  );
+}
