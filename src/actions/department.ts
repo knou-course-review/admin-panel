fe51559842e79646e147841c0ba8cafdc16346d7
jbilee <julishahn@gmail.com>
2024-08-06 10:39:31 +0900

    feat: 학과 데이터를 수정하는 서버 액션 추가

--- src/actions/department.ts
+++ src/actions/department.ts
@@ -32,6 +32,33 @@ export async function addDepartment(formData: FormData) {
   return { isValid: false };
 }
 
+export async function updateDepartment(departmentId: number, formData: any) {
+  // Return early if not admin
+  const userSession = await getSession();
+  // TODO: Decrypt token here and check expiry & admin role
+  if (!userSession) return { isValid: false };
+
+  const validatedForm = DepartmentFormSchema.safeParse(formData);
+  if (!validatedForm.success) {
+    return { errors: validatedForm.error.flatten().fieldErrors };
+  }
+
+  if (validatedForm.success) {
+    const data = validatedForm.data;
+    try {
+      const res = await api.put(`/api/v1/department/${departmentId}`, data, userSession.token);
+      const body = await res.json();
+      console.log(body);
+      if (body.code === 200) {
+        return { isValid: true, data: body.data };
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return { isValid: false };
+}
+
 export async function deleteDepartment(departmentId: string) {
   // Return early if not admin
   const userSession = await getSession();
