856428399cd3cb486561d4ca44d577a4ee26bd9d
jbilee <julishahn@gmail.com>
2024-08-06 11:34:56 +0900

    refactor: 로그인 페이지 UI 개선 및 메인홈 컨텐츠 수정 - 로그인 폼에 너비 제한 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,11 @@ import LoginForm from "@/components/login/LoginForm";
 
 export default function Login() {
   return (
-    <div className="flex flex-col gap-4 text-center">
-      <h1 className="font-bold text-2xl mb-8">로그인</h1>
-      <LoginForm />
+    <div className="flex justify-center pt-10">
+      <div className="flex flex-col w-96 gap-4 text-center">
+        <h1 className="font-bold text-2xl mb-8">로그인</h1>
+        <LoginForm />
+      </div>
     </div>
   );
 }
